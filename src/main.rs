64659435e7cab4788fc448fc15df389ebd506cf7
TTNguyen <nttriet9a4@gmail.com>
2022-05-23 12:04:19 +0700

    refactor code + update parsing func

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ pub enum FunctionType {
     READ,
     WRITE,
     PAYABLE,
+    INIT,
     UNKNOWN
 }
 
@@ -31,6 +32,13 @@ pub enum FunctionType {
 // #[private] -> Use for callback functions
 // #[payable] -> WRITE + PAYABLE Methods
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[serde(tag = "type")]
+pub enum FunctionTag {
+    Init,
+    Private,
+    Payable 
+}
 
 fn main() {
     //TODO: Get list methods name from near-contract-parse
@@ -41,31 +49,38 @@ fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
-    let mut is_payable = false;
-    let extracted_data: Vec<String> = reader.lines().into_iter().filter_map(|v| {
-            let v_unwrap = v.unwrap().trim().to_string();
-            if is_payable_function(v_unwrap.clone()) {
-                is_payable = true;
-            }
-            if is_pub_fn_parttern(v_unwrap.clone()) {
-                let concat_str = if is_payable {
-                    is_payable = false;
-                    v_unwrap + "#payable"
-                } else {
-                    v_unwrap
-                };
-                Some(concat_str)
-            } else {
-                    None
-            }
-        }
-    ).collect();
+    let mut extracted_data: Vec<ContractFunction> = vec![];
+    let mut holding_tag: Option<FunctionTag> = None;
+    for (_index, data) in reader.lines().into_iter().enumerate() {
+        let unwraped_data = data.unwrap();
+        let trimmed_data = unwraped_data.trim();
 
-    for func in extracted_data {
-        println!("{:#?}", func);
-        parse_contract_methods(func);
+        if is_pub_fn_parttern(trimmed_data.to_string()) {
+            extracted_data.push(parse_contract_methods(trimmed_data.to_string(), holding_tag).unwrap()); //TODO: Handle err
+        }
+
+        holding_tag = check_tag_line(trimmed_data.to_string());  
     }
 
+    for i in extracted_data {
+        println!("{:#?}", i);
+    }
+
+    // let json_object = serde_json::to_string(&contract_fn).unwrap();
+    // println!("Contract-fn: {} \n\n", json_object);
+    // contract_fn
+}
+
+fn check_tag_line(line: String) -> Option<FunctionTag> {
+    if line == "#[init]" {
+        Some(FunctionTag::Init)
+    } else if line == "#[payable]" {
+        Some(FunctionTag::Payable)
+    } else if line == "#[private]" {
+        Some(FunctionTag::Private) 
+    } else {
+        None
+    }
 }
 
 fn is_pub_fn_parttern(line: String) -> bool {
@@ -76,32 +91,70 @@ fn is_pub_fn_parttern(line: String) -> bool {
     false
 }
 
-fn is_payable_function(line: String) -> bool {
-    line.trim() == "#[payable]"
+fn parse_contract_methods(line: String, tag: Option<FunctionTag>) -> Option<ContractFunction> {
+    if let Some(tag) = tag {
+        match tag {
+            FunctionTag::Private => None,
+            FunctionTag::Init | FunctionTag::Payable => {
+                let skip = 1;
+
+                let params_start = line.find('(').unwrap();
+                let params_end = line.find(')').unwrap();
+
+                let fn_index = line.find("fn").unwrap();
+                let fn_name = line.substring(fn_index+ "fn".len(), params_start).trim();
+
+                let params = line.substring(params_start + 1, params_end);
+                let params_list: Vec<&str> = params.split(',').collect();
+
+                let fn_type = if tag == FunctionTag::Init {
+                    FunctionType::INIT
+                } else {
+                    FunctionType::PAYABLE
+                };
+
+                Some(ContractFunction {
+                    name: fn_name.to_string(),
+                    return_type: "".to_string(),
+                    params: parse_params(params_list, skip),
+                    fn_type 
+                })
+            }, 
+        }
+    } else {
+        let skip= if line.contains("self") { //Handle 3 cases: &mut self, self, &self
+            1
+        } else {
+            0
+        };
+
+        let params_start = line.find('(').unwrap();
+        let params_end = line.find(')').unwrap();
+
+        let fn_index = line.find("fn").unwrap();
+        let fn_name = line.substring(fn_index+ "fn".len(), params_start).trim();
+
+        let params = line.substring(params_start + 1, params_end);
+        let params_list: Vec<&str> = params.split(',').collect();
+
+        let fn_type = if line.contains("&mut self") {
+            FunctionType::WRITE
+        } else {
+            FunctionType::READ
+        };
+
+        Some(ContractFunction {
+            name: fn_name.to_string(),
+            return_type: "".to_string(),
+            params: parse_params(params_list, skip),
+            fn_type 
+        })
+    }
 }
 
-fn parse_contract_methods(line: String) {
-    //NOTE: FORMAT: pub fn name(&mut self, abc: String) -> Void {}
-    let params_start = line.find('(').unwrap();
-    let params_end = line.find(')').unwrap();
-
-    let fn_index = line.find("fn").unwrap();
-    let fn_name = line.substring(fn_index+ "fn".len(), params_start).trim();
-
-    let params = line.substring(params_start + 1, params_end);
-    let params_list: Vec<&str> = params.split(',').collect();
-    let function_type = if line.contains(&"#payable") {
-        FunctionType::PAYABLE
-    } else if params_list[0] == "&mut self" {
-        FunctionType::WRITE 
-    } else if params_list[0] == "&self" {
-        FunctionType::READ
-    } else {
-        FunctionType::UNKNOWN
-    };
-
+fn parse_params(params_list: Vec<&str>, skip: usize) -> Vec<ContractParam> {
     let mut final_params = vec![];
-    for param in params_list.iter().skip(1)  {
+    for param in params_list.iter().skip(skip) {
         
         let trimmed_param = param.trim();
         let single_param: Vec<&str> = trimmed_param.split(':').collect();
@@ -110,19 +163,10 @@ fn parse_contract_methods(line: String) {
             ContractParam {
                 name: single_param[0].trim().to_string(),
                 param_type: single_param[1].trim().to_string()
-            });
+            }
+        );
     }
-
-
-    let contract_fn = ContractFunction {
-        name: fn_name.to_string(),
-        return_type: "".to_string(),
-        params: final_params,
-        fn_type: function_type
-    };
-
-    let json_object = serde_json::to_string(&contract_fn).unwrap();
-    println!("Contract-fn: {} \n\n", json_object);
+    final_params
 }
 
 
